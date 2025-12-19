940ce85ba85503c7bdef2a45c180589549c017e0
sofiyasudhagar2024csbs <sofiyasudhagar.2024csbs@sece.ac.in>
2025-12-19 10:22:33 +0530

    Routes changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Pages/Login';
-import Signup from './pages/Signup';
+import Signup from './Pages/Signup';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Meals from './pages/Meals';
@@ -29,46 +29,46 @@ function App() {
         <div className="App">
           {user && <Navbar user={user} onLogout={logout} />}
           <Routes>
-          <Route path="/" element={user ? <Home user={user} /> : <Navigate to="/login" replace />} />
-          <Route path="/login" element={user ? <Navigate to="/" replace /> : <Login />} />
-          <Route path="/signup" element={user ? <Navigate to="/" replace /> : <Signup />} />
-          <Route path="/profile" element={
-            <ProtectedRoute user={user}>
-              <Profile user={user} onLogout={logout} />
-            </ProtectedRoute>
-          } />
-          <Route path="/meals" element={
-            <ProtectedRoute user={user}>
-              <SimpleMeals />
-            </ProtectedRoute>
-          } />
-          <Route path="/workouts" element={
-            <ProtectedRoute user={user}>
-              <Workouts user={user} />
-            </ProtectedRoute>
-          } />
-          <Route path="/progress" element={
-            <ProtectedRoute user={user}>
-              <Progress user={user} />
-            </ProtectedRoute>
-          } />
-          <Route path="/trainers" element={
-            <ProtectedRoute user={user}>
-              <TrainerSelection />
-            </ProtectedRoute>
-          } />
-          <Route path="/trainer-dashboard" element={
-            <ProtectedRoute user={user}>
-              <TrainerDashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/guidance" element={
-            <ProtectedRoute user={user}>
-              <Guidance />
-            </ProtectedRoute>
-          } />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+            <Route path="/" element={user ? <Home user={user} /> : <Navigate to="/login" replace />} />
+            <Route path="/login" element={user ? <Navigate to="/" replace /> : <Login />} />
+            <Route path="/signup" element={user ? <Navigate to="/" replace /> : <Signup />} />
+            <Route path="/profile" element={
+              <ProtectedRoute user={user}>
+                <Profile user={user} onLogout={logout} />
+              </ProtectedRoute>
+            } />
+            <Route path="/meals" element={
+              <ProtectedRoute user={user}>
+                <SimpleMeals />
+              </ProtectedRoute>
+            } />
+            <Route path="/workouts" element={
+              <ProtectedRoute user={user}>
+                <Workouts user={user} />
+              </ProtectedRoute>
+            } />
+            <Route path="/progress" element={
+              <ProtectedRoute user={user}>
+                <Progress user={user} />
+              </ProtectedRoute>
+            } />
+            <Route path="/trainers" element={
+              <ProtectedRoute user={user}>
+                <TrainerSelection />
+              </ProtectedRoute>
+            } />
+            <Route path="/trainer-dashboard" element={
+              <ProtectedRoute user={user}>
+                <TrainerDashboard />
+              </ProtectedRoute>
+            } />
+            <Route path="/guidance" element={
+              <ProtectedRoute user={user}>
+                <Guidance />
+              </ProtectedRoute>
+            } />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </Router>
